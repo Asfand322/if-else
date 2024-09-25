6a35e74a4d8618d9c84695b8d44d1440f20d3965
Asfand322 <asfandsheikh321@gmail.com>
2024-09-25 13:54:26 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var fruits = prompt("enter your fruit name")
+var name = prompt("enter your name")
+if(fruits=="apple" || fruits=="Apple" && name=="asfand") {
+    document.write("apple is availabe")
+    console.log("apple is available")
+}
+else if(fruits=="mango") {
+    document.write("mango is sold out")
+    console.log("mango is sold out")
+}
+else {
+    document.write("wrong entry")
+    console.log("wrong entry")
+}
+    
